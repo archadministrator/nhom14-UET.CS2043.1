fa3dbe61ac82d05d751db73b3348a4c3fc2e64dd
archadmin <archadmin@dancingman.dancingman>
2026-04-01 16:45:06 +0700

    30% Week6

---
+++ homework4-10/SolutionDVH/Week6/Bai1.java
@@ -0,0 +1,44 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            AppConfig config = AppConfig.getInstance();
+            System.out.println(Thread.currentThread().getName() + " - " + config.hashCode());
+        };
+
+        Thread t1 = new Thread(task, "Thread 1");
+        Thread t2 = new Thread(task, "Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
+class AppConfig {
+    public static volatile AppConfig instance;
+
+    private String appName;
+    private String version;
+    private String logLevel;
+
+    private AppConfig(){
+        this.appName = "App Name";
+        this.version = "1.21.11";
+        this.logLevel = "CEBR";
+    }
+
+    public static AppConfig getInstance(){
+        if (instance == null) { // Check trong
+            synchronized (AppConfig.class) {
+                if (instance == null) {
+                    instance = new AppConfig();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+
+
+
---
+++ homework4-10/SolutionDVH/Week6/Bai3.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose configuration - mac/win?: ");
+        String type = scanner.nextLine();
+
+        UIFactory factory = null;
+        if (type.equals("win")){
+            factory = new WindowsFactory();
+        } else if (type.equals("mac")){
+            factory = new MacFactory();
+        } else {
+            System.out.println("Invalid input");
+        }
+        scanner.close();
+
+        Button button = factory.createButton();
+        Checkbox checkbox = factory.createCheckbox();
+
+        button.render();
+        checkbox.render();
+    }
+}
+
+
+
+interface Button {void render();}
+interface Checkbox {void render();}
+
+interface UIFactory{
+    Button createButton();
+    Checkbox createCheckbox();
+}
+
+
+// WINDOWS factory
+class WindowsButton implements Button {
+    public void render(){
+        System.out.println("Rendered Windows button");
+    }
+}
+class WindowsCheckbox implements Checkbox {
+    public void render(){
+        System.out.println("Rendered Windows checkbox");
+    }
+}
+
+class WindowsFactory implements UIFactory {
+    public Button createButton(){
+        return new WindowsButton();
+    }
+    public Checkbox createCheckbox(){
+        return new WindowsCheckbox();
+    }
+}
+
+
+//Mac factory 
+class MacButton implements Button {
+    public void render(){
+        System.out.println("Rendered Mac button");
+    }
+}
+class MacCheckbox implements Checkbox {
+    public void render(){
+        System.out.println("Rendered Mac checkbox");
+    }
+}
+class MacFactory implements UIFactory {
+    public Button createButton(){
+        return new MacButton();
+    }
+    public Checkbox createCheckbox(){
+        return new MacCheckbox();
+    }
+}
