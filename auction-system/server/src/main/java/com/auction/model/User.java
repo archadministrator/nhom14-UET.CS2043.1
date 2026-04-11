89412c0a790d31dcf5957bcc55cb99d28c22728f
Duong Huy <dvhclssq@gmail.com>
2026-04-11 15:11:53 +0700

    Project - First structure

---
+++ auction-system/server/src/main/java/com/auction/model/User.java
@@ -0,0 +1,70 @@
+package com.auction.model;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.auction.model.enums.Role;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String email;
+    private Role role;
+    private BigDecimal balance;
+    private LocalDateTime createdAt;
+    private boolean isActive;
+
+    public User(){
+        this.balance = BigDecimal.ZERO;
+        this.createdAt = LocalDateTime.now();
+        this.isActive = true;
+    }
+
+    public User(long id, String username, String email,String password, Role role){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.balance = BigDecimal.ZERO;
+        this.createdAt = LocalDateTime.now();
+        this.isActive = true;
+    }
+
+    //Getter, Setter
+    public long getId(){return id;}
+    public String getUsername(){return username;}
+    public String getPassword(){return password;}
+    public String getEmail(){return email;}
+    public String getRole(){return role;}
+    public BigDecimal getBalance(){return balance;}
+    public LocalDateTime getCreatedAt(){return createdAt;}
+    public boolean isActive(){return isActive;}
+    
+    public void setId(long id){this.id = id;}
+    public void setUsername(String username){this.username = username;}
+    public void setEmail(String email){this.email = email;}
+    public void setPassword(String password){this.password = password;}
+    public void setRole(Role role){this.role = role;}
+    public void setBalance(BigDecimal balance){this.balance = balance;}
+
+    public void deposit(BigDecimal amount){
+        if (amount.compareTo(BigDecimal.ZERO)>0){
+            this.balance = this.balance.add(amount);
+        }
+    }
+
+    public boolean withdraw(BigDecimal amount){
+        if (amount.compareTo(BigDecimal.ZERO)>0){
+            if (this.balance.compareTo(amount)<0){
+                return false;
+            } 
+            this.balance = this.balance.subtract(amount);
+            return true;
+        } 
+        return false;
+    }
+
+}
---
+++ auction-system/server/src/main/java/com/auction/model/enums/Role.java
@@ -0,0 +1,5 @@
+package com.auction.model.enums;
+
+public enum Role {
+    ADMIN, BIDDER, SELLER
+}
