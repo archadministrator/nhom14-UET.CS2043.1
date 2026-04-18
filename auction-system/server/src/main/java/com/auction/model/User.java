54e5961f6027e1bb23d4edeeca0a073e95440ec6
25020302-LeCongNhat <25020302@vnu.edu.vn>
2026-04-18 23:37:58 +0700

    feat: add User model

--- auction-system/server/src/main/java/com/auction/model/User.java
+++ auction-system/server/src/main/java/com/auction/model/User.java
@@ -1,6 +1,5 @@
 package com.auction.model;
 
-
 import com.auction.model.enums.Role;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,13 +14,13 @@ public class User {
     private final LocalDateTime createdAt;
     private boolean isActive;
 
-    public User(){
+    public User() {
         this.balance = BigDecimal.ZERO;
         this.createdAt = LocalDateTime.now();
         this.isActive = true;
     }
 
-    public User(Long id, String username, String email,String password, Role role){
+    public User(Long id, String username, String email, String password, Role role) {
         this.id = id;
         this.username = username;
         this.email = email;
@@ -32,39 +31,81 @@ public class User {
         this.isActive = true;
     }
 
-    //Getter, Setter
-    public Long getId(){return id;}
-    public String getUsername(){return username;}
-    public String getPassword(){return password;}
-    public String getEmail(){return email;}
-    public Role getRole(){return role;}
-    public BigDecimal getBalance(){return balance;}
-    public LocalDateTime getCreatedAt(){return createdAt;}
-    public boolean isActive(){return isActive;}
-    
-    public void setId(Long id){this.id = id;}
-    public void setUsername(String username){this.username = username;}
-    public void setEmail(String email){this.email = email;}
-    public void setPassword(String password){this.password = password;}
-    public void setRole(Role role){this.role = role;}
-    public void setBalance(BigDecimal balance){this.balance = balance;}
-    public void setActive(boolean active){this.isActive = active;}
+    // Getter, Setter
+    public Long getId() {
+        return id;
+    }
 
-    public void deposit(BigDecimal amount){
-        if (amount.compareTo(BigDecimal.ZERO)>0){
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void setActive(boolean active) {
+        this.isActive = active;
+    }
+
+    public void deposit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) > 0) {
             this.balance = this.balance.add(amount);
         }
     }
 
-    public boolean withdraw(BigDecimal amount){
-        if (amount.compareTo(BigDecimal.ZERO)>0){
-            if (this.balance.compareTo(amount)<0){
+    public boolean withdraw(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) > 0) {
+            if (this.balance.compareTo(amount) < 0) {
                 return false;
-            } 
+            }
             this.balance = this.balance.subtract(amount);
             return true;
-        } 
+        }
         return false;
     }
-
 }
